8c7bab7b43c43f42d9bb77cce360fb7a58ad3328
Ben <ben.curtis@zama.ai>
2022-05-13 14:08:48 +0100

    backend impl

--- concrete-core/src/backends/fftw/implementation/engines/glwe_secret_key_tensor_product.rs
+++ concrete-core/src/backends/fftw/implementation/engines/glwe_secret_key_tensor_product.rs
@@ -26,7 +26,6 @@ use crate::prelude::{LweBootstrapKeyEntity, PolynomialCount, PolynomialSize};
 use crate::prelude::numeric::CastInto;
 use crate::specification::engines::{TensorProductGlweSecretKeyCreationEngine,
                                     TensorProductGlweSecretKeyCreationError};
-use std::fmt::rt::v1::Count::Implied;
 
 /// # Description:
 /// Implementation of [`TensorProductGlweSecretKeyCreationEngine`] for [`FftwEngine`] that operates
@@ -59,29 +58,26 @@ for FftwEngine
         let input_list_1 = input_key1.0.as_polynomial_list();
         let input_list_2 = input_key2.0.as_polynomial_list();
 
-        let mut list = PolynomialList::allocate(0 as u32,
+        let mut output_list = PolynomialList::allocate(0 as u32,
                                             PolynomialCount(input_key1.0.polynomial_size().0),
                                             input_key1.0.polynomial_size());
 
-
         // create iterators over the two polynomial lists, as well as an output
-        let mut iter_output = list.polynomial_iter_mut();
-        let iterator_1 = input_list_1.polynomial_iter();
-        let iterator_2 = input_list_2.polynomial_iter();
+        {
+            let mut iter_output = output_list.polynomial_iter_mut();
+            let iterator_1 = input_list_1.polynomial_iter();
 
-        // fill the output of the iterator up with the correct product/s
-        for (i, polynomial1) in iterator_1.enumerate() {
-            for (j, polynomial2) in iterator_2.enumerate(){
-                let mut output_poly1 = iter_output.next().unwrap();
-                // TODO: correct the below, we need s_i, s_is_j, s_i^2 terms in the same order
-                output_poly1.fill_with_karatsuba_mul(&polynomial1, &polynomial2);
+            // fill the output of the iterator up with the correct product/s
+            for (i, polynomial1) in iterator_1.enumerate() {
+                for (j, polynomial2) in input_list_2.polynomial_iter().enumerate() {
+                    let mut output_poly1 = iter_output.next().unwrap();
+                    // TODO: correct the below, we need s_i, s_is_j, s_i^2 terms in the same order
+                    output_poly1.fill_with_karatsuba_mul(&polynomial1, &polynomial2);
+                }
             }
         }
-
-        // put the tensor product key into a GlweSecretKey object
-        // TODO: correct the below
         let tensor_key =
-            ImplGlweSecretKey::binary_from_container(list,
+            ImplGlweSecretKey::binary_from_container(output_list.as_tensor().as_slice().to_vec(),
                                                      input_key1.0.polynomial_size());
 
 
