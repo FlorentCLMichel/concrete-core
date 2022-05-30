4420c11af9b70362c2a540b6f51e3f85cad68378
Ben <ben.curtis@zama.ai>
2022-05-30 16:57:15 +0200

    u64 spec key tensor prod

--- concrete-core/src/backends/fftw/implementation/engines/glwe_secret_key_tensor_product_same_key.rs
+++ concrete-core/src/backends/fftw/implementation/engines/glwe_secret_key_tensor_product_same_key.rs
@@ -22,7 +22,7 @@ use crate::prelude::{
 /// on 32 bits integers. It outputs a tensor product of the input GLWE secret keys in the standard
 /// domain.
 impl GlweSecretKeyTensorProductSameKeyEngine<GlweSecretKey32, GlweSecretKey32> for FftwEngine {
-    // TODO write public documentation
+    // TODO write public documentation (for both 32 and 64)
     fn create_tensor_product_glwe_secret_key_same_key(
         &mut self,
         input: &GlweSecretKey32,
@@ -38,4 +38,25 @@ impl GlweSecretKeyTensorProductSameKeyEngine<GlweSecretKey32, GlweSecretKey32> f
     }
 }
 
-// TODO add u64 implementations
+/// # Description:
+/// Implementation of [`GlweSecretKeyTensorProductSameKeyEngine`] for
+/// [`FftwEngine`] that operates
+/// on 64 bits integers. It outputs a tensor product of the input GLWE secret keys in the standard
+/// domain.
+impl GlweSecretKeyTensorProductSameKeyEngine<GlweSecretKey64, GlweSecretKey64> for FftwEngine {
+    // TODO write public documentation
+    fn create_tensor_product_glwe_secret_key_same_key(
+        &mut self,
+        input: &GlweSecretKey64,
+    ) -> Result<GlweSecretKey64, GlweSecretKeyTensorProductSameKeyError<Self::EngineError>> {
+        Ok(unsafe { self.create_tensor_product_glwe_secret_key_same_key_unchecked(input) })
+    }
+
+    unsafe fn create_tensor_product_glwe_secret_key_same_key_unchecked(
+        &mut self,
+        input: &GlweSecretKey64,
+    ) -> GlweSecretKey64 {
+        input.0.create_tensor_product_key()
+    }
+}
+
